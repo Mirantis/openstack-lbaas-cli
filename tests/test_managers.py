8a0057712cee8881f5b8fc0b6be3e63fcea2fd26
Ilya Kharin <akscram@gmail.com>
2012-07-05 18:58:06 +0400

    Add unit tests for NodeManager.

--- tests/test_managers.py
+++ tests/test_managers.py
@@ -4,6 +4,7 @@ import mock
 
 from balancerclient.common import client
 from balancerclient.v1.loadbalancers import LoadBalancerManager
+from balancerclient.v1.nodes import NodeManager
 
 
 class LoadBalancerManagerTest(unittest.TestCase):
@@ -64,3 +65,66 @@ class LoadBalancerManagerTest(unittest.TestCase):
                              'loadbalancers')
         self.assertTrue(mock_update.called)
         self.assertEqual(mock_update.mock_calls, [expected])
+
+
+class NodeManagerTest(unittest.TestCase):
+    def setUp(self):
+        self.nodes = NodeManager(mock.Mock())
+        self.node = mock.Mock(id='fakeid')
+        self.lb = mock.Mock(id='lbfakeid')
+
+    @mock.patch('balancerclient.common.base.Manager._create', autospec=True)
+    def test_create(self, mock_create):
+        self.nodes.create(self.lb, 'node1', 'HW', '10.0.0.1', 80, 10, 'ACTIVE')
+        body = {'node': {'name': 'node1',
+                         'type': 'HW',
+                         'address': '10.0.0.1',
+                         'port': 80,
+                         'weight': 10,
+                         'status': 'ACTIVE'}}
+        expected = mock.call(self.nodes, '/loadbalancers/lbfakeid/nodes',
+                             body, 'node')
+        self.assertTrue(mock_create.called)
+        self.assertEqual(mock_create.mock_calls, [expected])
+
+    @mock.patch('balancerclient.common.base.Manager._update', autospec=True)
+    def test_update(self, mock_update):
+        self.nodes.update(self.lb, self.node, name='node1', type='HW',
+                          address='10.0.0.1', port=80, weight=10,
+                          status='ACTIVE')
+        body = {'name': 'node1',
+                'type': 'HW',
+                'address': '10.0.0.1',
+                'port': 80,
+                'weight': 10,
+                'status': 'ACTIVE'}
+        expected = mock.call(self.nodes,
+                             '/loadbalancers/lbfakeid/nodes/fakeid',
+                             body, 'nodes')
+        self.assertTrue(mock_update.called)
+        self.assertEqual(mock_update.mock_calls, [expected])
+
+    @mock.patch('balancerclient.common.base.Manager._update', autospec=True)
+    def test_update_status(self, mock_update):
+        self.nodes.update_status(self.lb, self.node, 'FAKESTATUS')
+        expected = mock.call(self.nodes,
+                             '/loadbalancers/lbfakeid/nodes/fakeid/FAKESTATUS',
+                             'loadbalancers')
+        self.assertTrue(mock_update.called)
+        self.assertEqual(mock_update.mock_calls, [expected])
+
+    @mock.patch('balancerclient.common.base.Manager._delete', autospec=True)
+    def test_delete(self, mock_delete):
+        self.nodes.delete(self.lb, self.node)
+        expected = mock.call(self.nodes,
+                             '/loadbalancers/lbfakeid/nodes/fakeid')
+        self.assertTrue(mock_delete.called)
+        self.assertEqual(mock_delete.mock_calls, [expected])
+
+    @mock.patch('balancerclient.common.base.Manager._list', autospec=True)
+    def test_nodes_for_lb(self, mock_list):
+        self.nodes.nodes_for_lb(self.lb)
+        expected = mock.call(self.nodes, '/loadbalancers/lbfakeid/nodes',
+                             'nodes')
+        self.assertTrue(mock_list.called)
+        self.assertEqual(mock_list.mock_calls, [expected])
